6583e10b2a0b8eaaaa0239cbee4ed6fffb45063a
gaomiingyue <752797754@qq.com>
2018-10-26 22:42:47 +0800

    搜索

---
+++ pages/search/search.js
@@ -0,0 +1,31 @@
+// pages/search/search.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    "friendlist": [],
+    "ishidden":true,
+    "searchinput":""
+  },
+  search: function (value, cursor, keyCode){
+    var friendlist = [];
+    this.setData({
+      "ishidden": value.detail.cursor==0
+    })
+    if (value.detail.value=="小雷"){
+      friendlist.push({ "id": "1", "name": "网小类", "subname": "小雷", "sex": "1" });
+      this.setData({
+        "friendlist": friendlist
+      })
+    }
+  },
+  reset:function(){
+    this.setData({
+      "searchinput": "",
+      "friendlist":[],
+      "ishidden":true
+    })
+  }
+})
