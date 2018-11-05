a48eb8ecee505a77db43bd5adf4431008fef0d01
gaomiingyue <752797754@qq.com>
2018-11-06 00:36:24 +0800

    the end

--- pages/mail/edit.js
+++ pages/mail/edit.js
@@ -28,7 +28,9 @@ Page({
     },1000)
     App.request('mail/code', {"mail":this.data.mail}, function (data) {
       if (data.code == "success") {
-        App.toast("发送成功！");
+        wx.showModal({
+          content: "发送成功，验证码有效期为5分钟！请确保发件方在白名单内。", showCancel: false
+        });
       } else {
         App.toast("发送失败！");
       }
@@ -36,8 +38,17 @@ Page({
   },
   bindsubmit: function (e) {
     e.detail.value["formId"] = e.detail.formId;
+    if (!/^(\w-*\.*)+@(\w-?)+(\.\w{2,})+$/.test(e.detail.value.mail)) {
+      App.toast("邮箱不正确！");
+      return;
+    }
+    if (e.detail.value.yzm.length<4) {
+      App.toast("请输入验证码！");
+      return;
+    }
     wx.showLoading({ title: '正在提交...', mask: true });
-    App.request('mail/confirm', {"mail":this.data.mail,"yzm":this.data.yzm}, function (data) {
+    App.request('mail/confirm', e.detail.value, function (data) {
+      console.log(data)
       if (data.code == "success") {
         App.toast("绑定成功！", "success", function () {
           wx.navigateBack({ delta: 1 })
